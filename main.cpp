f7eb6fa0c4df7c745588d5867e02183bcb5fa3c4
ab-x-cd <nathseitz7@gmail.com>
2026-04-03 08:26:59 +0200

    test ?

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
+#ifdef QT_FOUND
 #include <QApplication>
+#include "src/view/QtView.h"
+#endif
+
 #include <memory>
 #include "src/controller/ScannerController.h"
-#include "src/view/QtView.h"
 
 int main(int argc, char* argv[]) {
-    // Mode console si des arguments sont passés
+    // Mode console (CLI) - utilisé si des arguments sont passés ou si Qt n'est pas disponible
     if (argc > 1) {
         auto model = std::make_shared<ScannerModel>();
         ScannerController controller(model);
         return controller.run(argc, argv);
     }
     
-    // Mode GUI par défaut
+#ifdef QT_FOUND
+    // Mode GUI par défaut (si Qt est disponible)
     QApplication app(argc, argv);
     
     // Créer le modèle et le contrôleur
@@ -23,4 +27,11 @@ int main(int argc, char* argv[]) {
     window.show();
     
     return app.exec();
+#else
+    // Si Qt n'est pas disponible, lancer le mode CLI avec usage
+    auto model = std::make_shared<ScannerModel>();
+    ScannerController controller(model);
+    const char* usage[] = {argv[0]};
+    return controller.run(1, (char**)usage);
+#endif
 }
